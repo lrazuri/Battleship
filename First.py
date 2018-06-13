588b8410116dca516d54bed0745dea5bcae9a498
Leo R <Razurileonardo@gmail.com>
2018-06-13 01:57:31 -0700

    added diff ships

--- First.py
+++ First.py
@@ -2,6 +2,9 @@ from random import randint
 
 def B_Ship_game(Dimx,Dimy):
     Players=input("How many people are playing?(1 or 2 only) ")
+    ships=["Carrier","Battleship","Cruiser","Submarine","Destroyer"]
+    shipspace=[5,4,3,3,2]
+    initials=["C","B","C","S","D"]
     if int(Players) == 1:
         board = []
         for x in range(0, Dimx):
@@ -47,20 +50,37 @@ def B_Ship_game(Dimx,Dimy):
                 print_board(board)             
     else:
         board1 = []
+        Rboard1= []
         board2 = []
+        Rboard2= []
         for x in range(0, Dimx):
           board1.append(["O"] * Dimy)
+          Rboard1.append(["O"] * Dimy)          
           board2.append(["O"] * Dimy)
+          Rboard2.append(["O"] * Dimy)
         def print_board(board):
             for row in board:
                 print (" ".join(row))
-        print("Player 1 guess now ")
-        P1boat_row=int(input("Which row will your boat be in? "))
-        P1boat_col=int(input("Which collumn will your boat be in? "))
+        print("Player 1 place ships now:")
+        for ship,space,ini in zip(ships,shipspace,initials):
+            print(str(ship)+"s have "+str(space)+" spots where do you want to put it")
+            for i in range(space):
+                print("Place point " + str(i+1) + " of ship")
+                P1boat_row=int(input("Which row will your boat be in? "))
+                P1boat_col=int(input("Which collumn will your boat be in? "))
+                Rboard1[int(P1boat_row)][int(P1boat_col)] = ini
+                print_board(Rboard1)
         print ("\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n")
         print('Let player 2 choose their ships location now')
-        P2boat_row=int(input("Which row will your boat be in? "))
-        P2boat_col=int(input("Which collumn will your boat be in? "))
+        print("Player 2 place ships now:")
+        for ship,space,ini in zip(ships,shipspace,initials):
+            print(str(ship)+"s have "+str(space)+" spots where do you want to put it")
+            for i in range(space):
+                print("Place point " + str(i+1) + " of ship")
+                P2boat_row=int(input("Which row will your boat be in? "))
+                P2boat_col=int(input("Which collumn will your boat be in? "))
+                Rboard2[int(P2boat_row)][int(P2boat_col)] = ini
+                print_board(Rboard2)
         print ("\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n")
         
         for turn in range(Dimx**2):
@@ -76,9 +96,10 @@ def B_Ship_game(Dimx,Dimy):
                     guess_row = input("Guess Row: ")
                     guess_col = input("Guess Col: ")
                                                
-                if int(guess_row) == P2boat_row and int(guess_col) == P2boat_col:
-                    print ("Congratulations! You sank my battleship!")  
-                    break
+                if Rboard2[int(guess_row)][int(guess_col)] != "O":
+                    print ("You hit one of my ships")
+                    board2[int(guess_row)][int(guess_col)] = "H"
+                    print_board(board2) 
                 else: 
                     print ("You missed my battleship!")
                     board2[int(guess_row)][int(guess_col)] = "X"
@@ -95,9 +116,10 @@ def B_Ship_game(Dimx,Dimy):
                     guess_row = input("Guess Row: ")
                     guess_col = input("Guess Col: ")
                                                
-                if int(guess_row) == P1boat_row and int(guess_col) == P1boat_col:
-                    print ("Congratulations! You sank my battleship!")  
-                    break
+                if Rboard1[int(guess_row)][int(guess_col)] != "O":
+                    print ("You hit one of my ships")
+                    board1[int(guess_row)][int(guess_col)] = "H"
+                    print_board(board1)
                 else: 
                     print ("You missed my battleship!")
                     board1[int(guess_row)][int(guess_col)] = "X"
