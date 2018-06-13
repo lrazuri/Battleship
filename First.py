891dcb092d87814c1b651a10b1f01ce74e59d3d6
Leo R <Razurileonardo@gmail.com>
2018-06-13 02:39:05 -0700

    Added player validation and turn validation

--- First.py
+++ First.py
@@ -1,24 +1,40 @@
 from random import randint
 
+#Makes the board
+def Board_maker(Dimx,Dimy):
+    board = []
+    for x in range(0, Dimx):
+        board.append(["O"] * Dimy)
+    return (board)
+
+#prints the boards
+def print_board(board):
+    for row in board:
+        print (" ".join(row))
+
 def B_Ship_game(Dimx,Dimy):
+    #Checks how many players
     Players=input("How many people are playing?(1 or 2 only) ")
+    while Players.isnumeric()==False or int(Players) >= 3: 
+        print("Type the numbers 1 or 2")
+        Players=input("How many people are playing?(1 or 2 only)")    
+    #Checks how many turns each player
+    Turn_number=input("How many turns will each player get? ")
+    while Turn_number.isnumeric()==False or int(Turn_number) <= 17 or int(Turn_number)>Dimx*Dimy: 
+        print("Made a mistake, Type a number that is greater than 17 or less than "+str(Dimx*Dimy))
+        Turn_number=input("How many turns will each player get? ")
+        
     ships=["Carrier","Battleship","Cruiser","Submarine","Destroyer"]
     shipspace=[5,4,3,3,2]
     initials=["C","B","C","S","D"]
+    
+    #one player game
     if int(Players) == 1:
-        board = []
-        for x in range(0, Dimx):
-          board.append(["O"] * Dimy)
-        
-        def print_board(board):
-            for row in board:
-                print (" ".join(row))
-        
+        board = Board_maker(Dimx,Dimy)
         print_board(board)
         
         def random_row(board):
             return randint(0, len(board) - 1)
-        
         def random_col(board):
             return randint(0, len(board[0]) - 1)
         
@@ -27,8 +43,6 @@ def B_Ship_game(Dimx,Dimy):
         print (ship_row)
         print (ship_col)
         
-        # Everything from here on should be in your for loop
-        # don't forget to properly indent!
         for turn in range(Dimx**2):
             print ("Turn", turn + 1)
             guess_row = input("Guess Row: ")
@@ -47,20 +61,14 @@ def B_Ship_game(Dimx,Dimy):
             else: 
                 print ("You missed my battleship!")
                 board[int(guess_row)][int(guess_col)] = "X"
-                print_board(board)             
+                print_board(board)     
+                
+    #2 player game
     else:
-        board1 = []
-        Rboard1= []
-        board2 = []
-        Rboard2= []
-        for x in range(0, Dimx):
-          board1.append(["O"] * Dimy)
-          Rboard1.append(["O"] * Dimy)          
-          board2.append(["O"] * Dimy)
-          Rboard2.append(["O"] * Dimy)
-        def print_board(board):
-            for row in board:
-                print (" ".join(row))
+        board1 = Board_maker(Dimx,Dimy)
+        Rboard1= Board_maker(Dimx,Dimy)
+        board2 = Board_maker(Dimx,Dimy)
+        Rboard2= Board_maker(Dimx,Dimy)
         print("Player 1 place ships now:")
         for ship,space,ini in zip(ships,shipspace,initials):
             print(str(ship)+"s have "+str(space)+" spots where do you want to put it")
@@ -83,7 +91,7 @@ def B_Ship_game(Dimx,Dimy):
                 print_board(Rboard2)
         print ("\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n" "\n")
         
-        for turn in range(Dimx**2):
+        for turn in range(Turn_number*2):
             if turn % 2==0:
                 print ("Player 1, Turn", turn//2 + 1)
                 guess_row = input("Guess Row: ")
